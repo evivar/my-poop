1238626a4ce47853277f48132f82568570c2ddb2
ernestovivar <ernesto.vivar@tawk.to>
2026-04-16 10:01:34 +0800

    fix: update layout height class for improved responsiveness

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="h-screen flex flex-col">
+  <div class="h-dvh flex flex-col">
     <AppNavbar />
     <main class="flex-1 min-h-0" :class="isMapPage ? 'overflow-hidden' : 'overflow-y-auto'">
       <slot />
